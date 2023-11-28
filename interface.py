dcf94c93318ed1bb1947f0ff7927bb3ecbdf39a2
Kiran <116376429+kiranvshah@users.noreply.github.com>
2023-11-28 10:18:58 +0000

    make comboboxes readonly removes need for submit button and unit validation

--- interface.py
+++ interface.py
@@ -16,8 +16,6 @@ def switch_unit(unit):
             return 'kelvin'
         case '°R':
             return 'rankine'
-        case _:
-            return 'invalid'
 
 
 class TemperatureConverter(tk.Frame):
@@ -31,14 +29,13 @@ class TemperatureConverter(tk.Frame):
         # row 0
         self.original_temp_label = tk.Label(self, text='Original temp:')
         self.temp_input = tk.Entry(self, width=10, font=default_font)
-        self.original_unit_input = ttk.Combobox(self, values=UNITS, width=5, font=default_font)
+        self.original_unit_input = ttk.Combobox(self, values=UNITS, width=5, font=default_font, state='readonly')
         self.original_unit_input.current(0)  # set to C by default
         # row 1
         self.convert_to_label = tk.Label(self, text='Convert to:')
-        self.destination_unit_input = ttk.Combobox(self, values=UNITS, width=5, font=default_font)
+        self.destination_unit_input = ttk.Combobox(self, values=UNITS, width=5, font=default_font, state='readonly')
         self.destination_unit_input.current(1)  # set to F by default
         # row 2
-        self.convert_btn = tk.Button(self, text='Convert', command=self.run_convert_temp)
         self.result_label = tk.Label(self, text='')
 
         # place widgets in frame
@@ -60,8 +57,7 @@ class TemperatureConverter(tk.Frame):
         self.original_unit_input.grid(row=0, column=2, sticky=tk.W, **padding)
         self.convert_to_label.grid(row=1, column=0, sticky=tk.E, **padding)
         self.destination_unit_input.grid(row=1, column=1, sticky=tk.W, **padding)
-        self.convert_btn.grid(row=2, column=0, sticky=tk.W, **padding)
-        self.result_label.grid(row=2, column=1, columnspan=2, sticky=tk.W, **padding)
+        self.result_label.grid(row=2, column=0, columnspan=2, sticky=tk.W, **padding)
 
     def run_convert_temp(self):
         original_temp_str = self.temp_input.get()
@@ -84,16 +80,9 @@ class TemperatureConverter(tk.Frame):
             result = ''
 
         if valid_temp:
-            if original_unit == 'invalid':
-                result = 'Invalid input unit'
-            elif destination_unit == 'invalid':
-                result = 'Invalid output unit'
-            elif original_temp_str == '':
-                result = ''
-            else:
-                destination_temp = convert_temp(original_temp, original_unit, destination_unit)
-                result = f'Temperature: {destination_temp:.2f} {self.destination_unit_input.get()}'
-                success = True
+            destination_temp = convert_temp(original_temp, original_unit, destination_unit)
+            result = f'Temperature: {destination_temp:.2f} {self.destination_unit_input.get()}'
+            success = True
 
         # set result_label text to result
         self.result_label.config(text=result, fg='black' if success else 'red')
